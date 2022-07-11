e4d5cb115334d6b5ebdd48a5522ea459705bc0df
huxiaoan <hu7006@outlook.com>
2022-07-11 16:38:37 +0800

    下线功能

--- C#/Program.cs
+++ C#/Program.cs
@@ -20,16 +20,18 @@ namespace HUSTwireless{
             string id = "";
             string pwd = "";
 
-            WriteLine($"[*]Going to login with:\n    Host:{loginHost}:{loginPort}\n    Redirect:{redirectHost}:{redirectPort}\n    ID:{id}");
-            string redirectRequestStr = $"GET / HTTP/1.1\r\nHost: {redirectHost}:{redirectPort}\r\n"+
-                "User-Agent: C Socket\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\r\n"+
-                "Accept-Language: zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2\r\nAccept-Encoding: gzip, deflate\r\n"+
-                "Connection: keep-alive\r\nUpgrade-Insecure-Requests: 1\r\n\r\n";
+            // WriteLine($"[*]Going to login with:\n    Host:{loginHost}:{loginPort}\n    Redirect:{redirectHost}:{redirectPort}\n    ID:{id}");
+            // string redirectRequestStr = $"GET / HTTP/1.1\r\nHost: {redirectHost}:{redirectPort}\r\n"+
+            //     "User-Agent: C Socket\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\r\n"+
+            //     "Accept-Language: zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2\r\nAccept-Encoding: gzip, deflate\r\n"+
+            //     "Connection: keep-alive\r\nUpgrade-Insecure-Requests: 1\r\n\r\n";
             
-            string? queryStr = infoRequest(redirectHost,redirectPort,redirectRequestStr);
-            if(queryStr != null){
-                if(0 == login(loginHost,loginPort,queryStr,id,pwd))return 0;
-            }
+            // string? queryStr = infoRequest(redirectHost,redirectPort,redirectRequestStr);
+            // if(queryStr != null){
+            //     if(0 == login(loginHost,loginPort,queryStr,id,pwd))return 0;
+            // }
+
+            logout(loginHost,loginPort);
             return -1;
         }
 
@@ -126,6 +128,86 @@ namespace HUSTwireless{
 
         }
 
+        static public int logout(string ip, int port){
+            byte[] response = new byte[2048];
+            string userIndex;
+            Socket? logoutSocket = createSocket(ip,port);
+            if(logoutSocket == null)return -1;
+
+            string logoutStr = "GET /eportal/redirectortosuccess.jsp HTTP/1.1\r\n"+
+                         $"Host: {ip}:{port}\r\nUser-Agent: C Socket\r\n"+
+                         "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8\r\n"+
+                         "Accept-Language: zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2\r\n"+
+                         "Accept-Encoding: gzip, deflate\r\n"+
+                         "Connection: keep-alive\r\n\r\n";
+
+            try{
+            logoutSocket.Send(Encoding.ASCII.GetBytes(logoutStr));
+            int byteReceive = logoutSocket.Receive(response);
+            string logoutResult = Encoding.ASCII.GetString(response, 0, byteReceive);
+
+            if(logoutResult.Contains("userIndex")){
+                logoutResult = logoutResult.Substring(logoutResult.IndexOf("userIndex="));
+                userIndex = logoutResult.Remove(logoutResult.IndexOf("\r\n"));
+
+                if(userIndex.Length > 256)throw new Exception("Incorrect response");
+                logoutSocket.Shutdown(SocketShutdown.Both);
+                logoutSocket.Close();
+
+            }else{
+                WriteLine("[*]Error requesting userIndex from {0}:{1}",ip,port);
+                throw new Exception("Incorrect response");
+            }
+
+            }catch(Exception e){
+                logoutSocket.Shutdown(SocketShutdown.Both);
+                logoutSocket.Close();
+                WriteLine($"[!]Error requesting userIndex:{e.ToString()}");
+                return -1;
+            }
+
+            return logoutWithUserIndex(ip,port,userIndex);
+
+
+            
+        }
+
+        static public int logoutWithUserIndex(string ip, int port, string userIndex){
+            byte[] response = new byte[2048];
+            Socket? logoutSocket = createSocket(ip,port);
+            if(logoutSocket == null)return -1;
+
+            string logoutStr = "POST /eportal/InterFace.do?method=logout HTTP/1.1\r\n"+
+                        $"Host: {ip}:{port}\r\nUser-Agent: C Socket\r\n"+
+                        "Accept: */*\r\n"+
+                        "Accept-Language: zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2\r\n"+
+                        "Accept-Encoding: gzip, deflate\r\n"+
+                        "Content-Type: application/x-www-form-urlencoded; charset=UTF-8\r\n"+
+                        $"Content-Length: {userIndex.Length}\r\n"+
+                        $"Origin: http://{ip}:{port}\r\n"+
+                        $"Connection: keep-alive\r\n\r\n{userIndex}";
+
+            try{
+                logoutSocket.Send(Encoding.ASCII.GetBytes(logoutStr));
+                int byteReceive = logoutSocket.Receive(response);
+                string logoutResult = Encoding.ASCII.GetString(response, 0, byteReceive);
+
+                if(logoutResult.Contains("success")){
+                    WriteLine("[*]Logout Successfully!");
+                    logoutSocket.Shutdown(SocketShutdown.Both);
+                    logoutSocket.Close();
+                    return 0;
+                }else{
+                    WriteLine("[!]Failed to logout");
+                    throw new Exception("unexpected response");
+                }
+            }catch(Exception e){
+                logoutSocket.Shutdown(SocketShutdown.Both);
+                logoutSocket.Close();
+                WriteLine($"[!]Error logging out: {e.ToString()}");
+                return -1;
+            }
+        } 
     }
 
 }
